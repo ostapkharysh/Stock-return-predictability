a66f04cf8a3dae77ec8f4cf10a365a03e21fb666
ostapkharysh <harysh@ucu.edu.ua>
2018-10-20 18:54:15 +0300

    Improvement of scrapping + DB methods

---
+++ db_manager.py
@@ -0,0 +1,38 @@
+# credits to http://www.sqlitetutorial.net
+
+
+import sqlite3
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return None
+
+
+def create_task(conn, task):
+    """
+    Create a new task
+    :param conn:
+    :param task:
+    :return:
+    """
+
+    sql = ''' INSERT INTO Reuters_news(date_time, title, article_text, link)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, task)
+    return cur.lastrowid
+
+
+def close_db(conn):
+    conn.close()
